da325cdc0eea0c94c8e011f274ae1704c359a9b1
xvisox <hubertmirek@gmail.com>
2024-04-20 21:25:44 +0200

    dziala

--- src/Typing/Exception.hs
+++ src/Typing/Exception.hs
@@ -6,11 +6,23 @@ import AbsSeeemcrd(Ident)
 import Common
 import Typing.Types
 
+data TypeCheckError = TypeCheckError Pos TypeCheckException
+
+instance Show TypeCheckError where
+  show (TypeCheckError pos ex) = show ex ++ " at " ++ showPos pos
+
 data TypeCheckException
-  = AssignmentMismatch TCType TCType Pos
+  = AssignmentMismatch TCType TCType
   | Redeclared Ident
   | NoVariable Ident
+  | NotAFunction Ident
+  | WrongArgumentCount Int Int
+  | ArgumentMismatch TCArgType TCArgType
 
 instance Show TypeCheckException where
-  show (AssignmentMismatch expected actual pos) = "Assignment mismatch: expected " ++ show expected ++ ", got " ++ show actual ++ " at " ++ showPos pos
+  show (AssignmentMismatch expected actual) = "Assignment mismatch: expected " ++ show expected ++ ", got " ++ show actual
   show (Redeclared ident) = "Redeclared variable: " ++ show ident
+  show (NoVariable ident) = "No such variable: " ++ show ident
+  show (NotAFunction ident) = "Not a function: " ++ show ident
+  show (WrongArgumentCount expected actual) = "Wrong argument count: expected " ++ show expected ++ ", got " ++ show actual
+  show (ArgumentMismatch expected actual) = "Argument mismatch: expected " ++ show expected ++ ", got " ++ show actual
--- src/Typing/TypeCheck.hs
+++ src/Typing/TypeCheck.hs
@@ -4,19 +4,35 @@ import Data.Map
 import AbsSeeemcrd
 import Control.Monad.Except
 import Control.Monad.Reader
-import Control.Monad(void)
+import Control.Monad
 
 import Common
 import Typing.Types
 import Typing.Exception
 import Typing.Environment
 
-type TCM = ReaderT Env (Except TypeCheckException)
+-- Typechecking monad
 
-typeCheck :: Program -> Either TypeCheckException ()
-typeCheck (PProgram _ topDefs) = runExcept $ runReaderT (void $ typeCheckTopDefs topDefs) initEnv
+type TCM = ReaderT Env (Except TypeCheckError)
+
+throwTypeCheckError :: Pos -> TypeCheckException -> TCM a
+throwTypeCheckError pos error = throwError $ TypeCheckError pos error
+
+declareIdentOrThrow :: Pos -> Ident -> TCType -> TCM Env
+declareIdentOrThrow pos ident varType = do
+  env <- ask
+  let currentScope = scope env
+  let maybeVar = lookupVar ident env
+  case maybeVar of
+    Just (_, varScope) -> if varScope == currentScope
+                            then throwTypeCheckError pos $ Redeclared ident
+                            else return env -- Shadowing allowed, so the identifier is not redeclared
+    Nothing -> return $ insertVar ident varType env
 
 -- Typechecking functions
+
+typeCheck :: Program -> Either TypeCheckError ()
+typeCheck (PProgram _ topDefs) = runExcept $ runReaderT (void $ typeCheckTopDefs topDefs) initEnv
 
 typeCheckTopDefs :: [TopDef] -> TCM Env
 typeCheckTopDefs [] = ask
@@ -35,27 +51,40 @@ typeCheckItems varType (item:items) = do
   local (const env) $ typeCheckItems varType items
 
 typeCheckItem :: TCType -> Item -> TCM Env
-typeCheckItem varType (NoInit _ ident) = do
-  env <- ask
-  if lookupVar ident env /= Nothing
-    then throwError $ Redeclared ident
-    else return $ insertVar ident varType env
+typeCheckItem varType (NoInit pos ident) = do
+  return =<< declareIdentOrThrow pos ident varType
 typeCheckItem varType (Init pos ident expr) = do
-  env <- ask
   exprType <- typeCheckExpr expr
   if exprType == varType
-    then if lookupVar ident env /= Nothing
-      then throwError $ Redeclared ident
-      else return $ insertVar ident varType env
-    else throwError $ AssignmentMismatch varType exprType pos
+    then return =<< declareIdentOrThrow pos ident varType
+    else throwTypeCheckError pos $ AssignmentMismatch varType exprType
 
 typeCheckExpr :: Expr -> TCM TCType
-typeCheckExpr (EVar _ ident) = do
+typeCheckExpr (EVar pos ident) = do
   env <- ask
   case lookupVar ident env of
     Just (varType, _) -> return varType
-    Nothing -> throwError $ NoVariable ident
+    Nothing -> throwTypeCheckError pos $ NoVariable ident
 typeCheckExpr (ELitInt _ _) = return TCInt
 typeCheckExpr (ELitTrue _) = return TCBool
 typeCheckExpr (ELitFalse _) = return TCBool
 typeCheckExpr (EString _ _) = return TCString
+typeCheckExpr (EApp pos ident exprs) = do
+  env <- ask
+  case lookupVar ident env of
+    Just (TCFun args returnType, _) -> do
+      if length args /= length exprs
+        then throwTypeCheckError pos $ WrongArgumentCount (length args) (length exprs)
+        else do
+          let argExprPairs = zip args exprs
+          forM_ argExprPairs $ \((TCArgType argKind argType), expr) -> do
+            exprType <- typeCheckExpr expr
+            exprKind <- case expr of
+              EVar _ _ -> return TCRef
+              _ -> return TCVal
+            if argType == exprType && (argKind == exprKind || argKind == TCVal)
+              then return ()
+              else throwTypeCheckError pos $ ArgumentMismatch (TCArgType argKind argType) (TCArgType exprKind exprType)
+          return returnType
+    Just _ -> throwTypeCheckError pos $ NotAFunction ident
+    Nothing -> throwTypeCheckError pos $ NoVariable ident
