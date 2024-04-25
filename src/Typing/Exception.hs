292cd11f159370deb7bdf8699fdd2e87328a4490
xvisox <hubertmirek@gmail.com>
2024-04-25 23:07:17 +0200

    help omg

--- src/Typing/Exception.hs
+++ src/Typing/Exception.hs
@@ -12,17 +12,19 @@ instance Show TypeCheckError where
   show (TypeCheckError pos ex) = show ex ++ " at " ++ showPos pos
 
 data TypeCheckException
-  = AssignmentMismatch TCType TCType
+  = TypeMismatch TCType TCType
   | Redeclared Ident
   | NoVariable Ident
   | NotAFunction Ident
   | WrongArgumentCount Int Int
   | ArgumentMismatch TCArgType TCArgType
+  | NoReturn Ident
 
 instance Show TypeCheckException where
-  show (AssignmentMismatch expected actual) = "Assignment mismatch: expected " ++ show expected ++ ", got " ++ show actual
+  show (TypeMismatch expected actual) = "Type mismatch: expected " ++ show expected ++ ", got " ++ show actual
   show (Redeclared ident) = "Redeclared variable: " ++ show ident
   show (NoVariable ident) = "No such variable: " ++ show ident
   show (NotAFunction ident) = "Not a function: " ++ show ident
   show (WrongArgumentCount expected actual) = "Wrong argument count: expected " ++ show expected ++ ", got " ++ show actual
   show (ArgumentMismatch expected actual) = "Argument mismatch: expected " ++ show expected ++ ", got " ++ show actual
+  show (NoReturn ident) = "No return statement in function: " ++ show ident
--- src/Typing/TypeCheck.hs
+++ src/Typing/TypeCheck.hs
@@ -22,12 +22,20 @@ declareIdentOrThrow :: Pos -> Ident -> TCType -> TCM Env
 declareIdentOrThrow pos ident varType = do
   env <- ask
   let currentScope = scope env
-  let maybeVar = lookupVar ident env
-  case maybeVar of
+  case lookupVar ident env of
     Just (_, varScope) -> if varScope == currentScope
                             then throwTypeCheckError pos $ Redeclared ident
                             else return env -- Shadowing allowed, so the identifier is not redeclared
     Nothing -> return $ insertVar ident varType env
+
+validateIdentOrThrow :: Pos -> Ident -> TCType -> TCM Env
+validateIdentOrThrow pos ident expectedType = do
+  env <- ask
+  case lookupVar ident env of
+    Just (varType, _) -> if varType /= expectedType
+                          then throwTypeCheckError pos $ TypeMismatch expectedType varType
+                          else return env
+    Nothing -> throwTypeCheckError pos $ NoVariable ident
 
 -- Typechecking functions
 
@@ -44,10 +52,14 @@ typeCheckTopDef :: TopDef -> TCM Env
 typeCheckTopDef (GlobalDef _ varType items) = do
   typeCheckItems (mapToTCType varType) items
 typeCheckTopDef (FnDef pos returnType ident args block) = do
-  env <- declareIdentOrThrow pos ident (TCFun (mapToTCArgTypes args) (mapToTCType returnType))
+  let returnType' = mapToTCType returnType
+  env <- declareIdentOrThrow pos ident (TCFun (mapToTCArgTypes args) returnType')
+
   let typesWithIdents = mapToTypesWithIdents args
   let argEnv = Prelude.foldl (\acc (varType, ident) -> insertVar ident varType acc) env typesWithIdents
-  local (const argEnv) $ typeCheckBlock block
+  env' <- local (const argEnv { hasReturn = False, returnType = returnType' }) $ typeCheckBlock block
+
+  unless (hasReturn env') $ throwTypeCheckError pos $ NoReturn ident
   return env
 
 typeCheckItems :: TCType -> [Item] -> TCM Env
@@ -63,7 +75,7 @@ typeCheckItem varType (Init pos ident expr) = do
   exprType <- typeCheckExpr expr
   if exprType == varType
     then return =<< declareIdentOrThrow pos ident varType
-    else throwTypeCheckError pos $ AssignmentMismatch varType exprType
+    else throwTypeCheckError pos $ TypeMismatch varType exprType
 
 typeCheckExpr :: Expr -> TCM TCType
 typeCheckExpr (EVar pos ident) = do
@@ -79,19 +91,17 @@ typeCheckExpr (EApp pos ident exprs) = do
   env <- ask
   case lookupVar ident env of
     Just (TCFun args returnType, _) -> do
-      if length args /= length exprs
-        then throwTypeCheckError pos $ WrongArgumentCount (length args) (length exprs)
-        else do
-          let argExprPairs = zip args exprs
-          forM_ argExprPairs $ \((TCArgType argKind argType), expr) -> do
-            exprType <- typeCheckExpr expr
-            exprKind <- case expr of
-              EVar _ _ -> return TCRef
-              _ -> return TCVal
-            if argType == exprType && (argKind == exprKind || argKind == TCVal)
-              then return ()
-              else throwTypeCheckError pos $ ArgumentMismatch (TCArgType argKind argType) (TCArgType exprKind exprType)
-          return returnType
+      when (length args /= length exprs) $
+        throwTypeCheckError pos $ WrongArgumentCount (length args) (length exprs)
+      let argExprPairs = zip args exprs
+      forM_ argExprPairs $ \((TCArgType argKind argType), expr) -> do
+        exprType <- typeCheckExpr expr
+        exprKind <- case expr of
+          EVar _ _ -> return TCRef
+          _ -> return TCVal
+        when (argType /= exprType || (argKind /= exprKind && argKind /= TCVal)) $
+          throwTypeCheckError pos $ ArgumentMismatch (TCArgType argKind argType) (TCArgType exprKind exprType)
+      return returnType
     Just _ -> throwTypeCheckError pos $ NotAFunction ident
     Nothing -> throwTypeCheckError pos $ NoVariable ident
 
@@ -109,4 +119,22 @@ typeCheckStmts (stmt:stmts) = do
 typeCheckStmt :: Stmt -> TCM Env
 typeCheckStmt (BStmt _ block) = typeCheckBlock block
 typeCheckStmt (DStmt _ topDef) = typeCheckTopDef topDef
-typeCheckStmt _ = ask -- TODO;
+typeCheckStmt (Ass pos ident expr) = do
+  exprType <- typeCheckExpr expr
+  validateIdentOrThrow pos ident exprType
+typeCheckStmt (Incr pos ident) = validateIdentOrThrow pos ident TCInt
+typeCheckStmt (Decr pos ident) = validateIdentOrThrow pos ident TCInt
+typeCheckStmt (Ret pos expr) = do
+  env <- ask
+  exprType <- typeCheckExpr expr
+  let expectedReturnType = returnType env
+  if exprType == expectedReturnType
+    then return env { hasReturn = True }
+    else throwTypeCheckError pos $ TypeMismatch expectedReturnType exprType
+typeCheckStmt (VRet pos) = do
+  env <- ask
+  let expectedReturnType = returnType env
+  if expectedReturnType == TCVoid
+    then return env { hasReturn = True }
+    else throwTypeCheckError pos $ TypeMismatch TCVoid expectedReturnType
+typeCheckStmt _ = throwTypeCheckError Nothing $ WrongArgumentCount 1 2
