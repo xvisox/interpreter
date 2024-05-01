b3103d24addcff87d1ebf19dcb5565031adc0603
xvisox <hubertmirek@gmail.com>
2024-05-01 18:45:47 +0200

    examples dir

--- run_tests.sh
+++ run_tests.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
+echo "Building interpreter"
 make > /dev/null 2>&1
 
-for file in ./examples/good/*.see; do
-    echo "Testing $file"
-    ./interpreter $file > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        echo "Test failed"
-        exit 1
-    fi
-done
+test_files() {
+    local dir_path=$1
+    local expected_status=$2
 
-for file in ./examples/bad/*.see; do
-    echo "Testing $file"
-    ./interpreter $file > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        echo "Test failed"
-        exit 1
-    fi
-done
+    for file in "$dir_path"/in/*.see; do
+        echo "Testing $file"
 
-for file in ./examples/typing/*.see; do
-    echo "Testing $file"
-    ./interpreter $file > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        echo "Test failed"
-        exit 1
-    fi
-done
+        ./interpreter "$file" > "$file.out" 2>&1
+        local status=$?
+
+        if [ $expected_status -eq 0 ] && [ $status -ne 0 ]; then
+            echo "Test failed: Expected success but got failure"
+            exit 1
+        elif [ $expected_status -ne 0 ] && [ $status -eq 0 ]; then
+            echo "Test failed: Expected failure but got success"
+            exit 1
+        fi
+
+        local expected_file="${dir_path}/out/${file##*/}.out"
+        diff -u "$expected_file" "$file.out"
+        if [ $? -ne 0 ]; then
+            echo "Test failed: Output mismatch"
+            exit 1
+        fi
+
+        rm "$file.out"
+    done
+}
+
+test_files "./examples/good" 0
+#test_files "./examples/in/bad" 1
+#test_files "./examples/in/typing" 1
+
+echo "All tests passed."
